78e891a443afcd3150178ae50ea4f0a5e6631d8e
flavian-jumba <flaviajumba@gmail.com>
2024-07-18 00:40:53 +0300

    Method chaining (Promises 2)

--- script.js
+++ script.js
@@ -5,28 +5,38 @@
 //2.CLEAN THE KITCHEN
 //3.TAKE OUT THE TRASH 
 
-//use of callbacks 
+//(b)Use of Promises
 
-function walkDog(Callback){
-    setTimeout(()=>{
-         console.log("Walk the dogs out") 
-         Callback()
-    },5000);
+//PENDING ->RESOLVED or REJECTED
+//new promise ((resolve, reject) => {asynchronous code}
+
+function walkDog(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{ 
+             resolve("Walk the dogs out")
+       },5000);
+    })
 }
-function cleanTheKitchen(Callback){
-    setTimeout(()=>{
-        console.log("Clean the kitchen")
-        Callback()
-    },3000);
+function cleanTheKitchen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+           resolve("Time to clean the kitchen")
+        },3000); 
+    })
 }
-function takeOutTrash(Callback){
+function takeOutTrash(){
+   return new Promise((resolve, reject) => {
     setTimeout(()=>(
-        console.log("Time to take out the trash")
-        Callback()
+        resolve("Time to take out the trash")
     ), 500);
+   })
 }
 walkDog(()=>{
     cleanTheKitchen(()=>{
         takeOutTrash(()=>{console.log("You've finished all the chores")})
     })
 })
+//method chaining 
+walkDog().then(value =>{console.log(value);return cleanTheKitchen()})
+         .then(value =>{console.log(value);return takeOutTrash()})
+          .then(value =>{console.log(value);console.log("You finished all the chores")}) 
